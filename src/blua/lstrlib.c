b0dd07efce4f4f0c79eac77e021e2aeace5a1d4f
Jisk <37682565+Jiskster@users.noreply.github.com>
2026-09-12 02:00:37 -0500

    Add string.split to blua
    Source: https://www.lua.org/pil/27.2.html

--- src/blua/lstrlib.c
+++ src/blua/lstrlib.c
@@ -825,6 +825,27 @@ static int str_format (lua_State *L) {
   return 1;
 }
 
+static int str_split (lua_State *L) {
+  const char *s = luaL_checkstring(L, 1);
+  const char *sep = luaL_checkstring(L, 2);
+  const char *e;
+  int i = 1;
+
+  lua_newtable(L);  /* result */
+
+  /* repeat for each separator */
+  while ((e = strchr(s, *sep)) != NULL) {
+    lua_pushlstring(L, s, e-s);  /* push substring */
+    lua_rawseti(L, -2, i++);
+    s = e + 1;  /* skip separator */
+  }
+
+  /* push last substring */
+  lua_pushstring(L, s);
+  lua_rawseti(L, -2, i);
+
+  return 1;  /* return the table */
+}
 
 static const luaL_Reg strlib[] = {
   {"byte", str_byte},
@@ -844,6 +865,7 @@ static const luaL_Reg strlib[] = {
   {"reverse", str_reverse},
   {"sub", str_sub},
   {"upper", str_upper},
+  {"split", str_split},
   {NULL, NULL}
 };
 
